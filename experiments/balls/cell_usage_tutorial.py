fixed small bug with initial covariance (which was not learned in log space)

experiments/balls/cell_usage_tutorial.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 import os
-from rkn_cell.RKNCell import elup1, RKNCell
+from rkn_cell.RKNCell import var_activation, RKNCell
 from util.Losses import mse, gaussian_nll
 import time as t
 nn = torch.nn
@@ -63,7 +63,7 @@ class MyEncoder(nn.Module):
         h = nn.functional.normalize(h, p=2, dim=-1, eps=1e-8)
         mean = self._mean_layer(h)
         log_var = self._log_var_layer(h)
-        var = elup1(log_var)
+        var = var_activation(log_var)
         return mean, var
 
 
@@ -101,7 +101,7 @@ class MyDecoder(nn.Module):
         for layer in self._hidden_layers_var:
             h_var = layer(h_var)
         log_var = self._out_layer_var(latent_cov)
-        var = elup1(log_var)
+        var = var_activation(log_var)
         return mean, var
 
 enc = MyEncoder(lod=15).to(device)

rkn/RKN.py
@@ -6,6 +6,7 @@ import time as t
 from rkn.Encoder import Encoder
 from rkn.Decoder import SplitDiagGaussianDecoder
 from rkn.RKNLayer import RKNLayer
+from rkn_cell.RKNCell import var_activation, var_activation_inverse
 from util.ConfigDict import ConfigDict
 from typing import Tuple
 
@@ -49,15 +50,16 @@ class RKN:
 
         # build (default) initial state
         self._initial_mean = torch.zeros(1, self._lsd).to(self._device)
-        self._icu = torch.nn.Parameter(self._initial_state_variance * torch.ones(1, self._lod).to(self._device))
-        self._icl = torch.nn.Parameter(self._initial_state_variance * torch.ones(1, self._lod).to(self._device))
+        log_ic_init = var_activation_inverse(self._initial_state_variance)
+        self._log_icu = torch.nn.Parameter(log_ic_init * torch.ones(1, self._lod).to(self._device))
+        self._log_icl = torch.nn.Parameter(log_ic_init * torch.ones(1, self._lod).to(self._device))
         self._ics = torch.zeros(1, self._lod).to(self._device)
 
         # params and optimizer
         self._params = list(self._enc.parameters())
         self._params += list(self._rkn_layer.parameters())
         self._params += list(self._dec.parameters())
-        self._params += [self._icu, self._icl]
+        self._params += [self._log_icu, self._log_icl]
 
         self._optimizer = optim.Adam(self._params, lr=self._learning_rate)
         self._shuffle_rng = np.random.RandomState(42)  # rng for shuffling batches
@@ -93,7 +95,8 @@ class RKN:
         self._optimizer.zero_grad()
 
         w, w_var = self._enc(obs_batch)
-        post_mean, post_cov  = self._rkn_layer(w, w_var, self._initial_mean, [self._icu, self._icl, self._ics])
+        post_mean, post_cov = self._rkn_layer(w, w_var, self._initial_mean,
+                                              [var_activation(self._log_icu), var_activation(self._log_icl), self._ics])
         out_mean, out_var = self._dec(post_mean, torch.cat(post_cov, dim=-1))
 
         loss = gaussian_nll(target_batch, out_mean, out_var)
@@ -155,7 +158,9 @@ class RKN:
                 torch_targets = torch.from_numpy(targets[cur_slice].astype(np.float32)).to(self._device)
 
                 w, w_var = self._enc(torch_obs)
-                post_mean, post_cov = self._rkn_layer(w, w_var, self._initial_mean, [self._icu, self._icl, self._ics])
+                post_mean, post_cov = self._rkn_layer(w, w_var, self._initial_mean,
+                                                      [var_activation(self._log_icu), var_activation(self._log_icl),
+                                                       self._ics])
                 out_mean, out_var = self._dec(post_mean, torch.cat(post_cov, dim=-1))
 
                 loss = gaussian_nll(torch_targets, out_mean, out_var)

rkn_cell/RKNCell.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 from util.ConfigDict import ConfigDict
-from typing import Iterable, Tuple, List
+from typing import Iterable, Tuple, List, Union
 nn = torch.nn
 
 
@@ -29,22 +29,22 @@ def dadbt(a: torch.Tensor, diag_mat: torch.Tensor, b: torch.Tensor) -> torch.Ten
     return bmv(a * b, diag_mat)
 
 
-def elup1(x: torch.Tensor) -> torch.Tensor:
+def var_activation(x: torch.Tensor) -> torch.Tensor:
     """
     elu + 1 activation faction to ensure positive covariances
     :param x: input
     :return: exp(x) if x < 0 else x + 1
     """
-    return torch.exp(x).where(x < 0.0, x + 1.0)
+    return torch.log(torch.exp(x) + 1.0)
 
 
-def elup1_inv(x: torch.Tensor) -> torch.Tensor:
+def var_activation_inverse(x: Union[float, np.ndarray]) -> Union[float, np.ndarray]:
     """
     inverse of elu+1, numpy only, for initialization
     :param x: input
     :return:
     """
-    return np.log(x) if x < 1.0 else (x - 1.0)
+    return np.log(np.exp(x) - 1.0)
 
 
 class RKNCell(nn.Module):
@@ -79,15 +79,12 @@ class RKNCell(nn.Module):
         self.c = config
         self._dtype = dtype
 
-
-
         self._build_transition_model()
 
     @property
     def _device(self):
         return self._tm_11_full.device
 
- #   @torch.jit.script_method
     def forward(self, prior_mean: torch.Tensor, prior_cov: Iterable[torch.Tensor],
                 obs: torch.Tensor, obs_var: torch.Tensor, obs_valid: torch.Tensor = None) -> \
             Tuple[torch.Tensor, Iterable[torch.Tensor], torch.Tensor, Iterable[torch.Tensor]]:
@@ -155,7 +152,7 @@ class RKNCell(nn.Module):
         self._coefficient_net = self._build_coefficient_net(self.c.trans_net_hidden_units,
                                                             self.c.trans_net_hidden_activation)
 
-        init_trans_cov = elup1_inv(self.c.trans_covar)
+        init_trans_cov = var_activation_inverse(self.c.trans_covar)
         # TODO: This is currently a different noise for each dim, not like in original paper (and acrkn)
         self._log_transition_noise = \
             nn.Parameter(nn.init.constant_(torch.empty(1, self._lsd, dtype=self._dtype), init_trans_cov))
@@ -185,7 +182,7 @@ class RKNCell(nn.Module):
         tm22 = tm22_full * self._band_mask
         tm22 += torch.eye(self._lod, device=self._device)[None, :, :]
 
-        trans_cov = elup1(self._log_transition_noise)
+        trans_cov = var_activation(self._log_transition_noise)
 
         return [tm11, tm12, tm21, tm22], trans_cov
 